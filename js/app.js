b07b81b7a83e46c826e3319a5dac3efd74732303
Ludmilla Victória Dellagnesse <87335554+ludhdellagnesse@users.noreply.github.com>
2021-11-09 20:53:30 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+// pop-up
+$(document).ready(function() {
+    $('.pop-up',).magnificPopup({ delegate: 'a',type:'image',
+    
+  });
+  });
+
+
+
+  $(document).ready(function() {
+    $('.link-mapa').magnificPopup({type:'iframe'});
+  })
+  // 
+
+  //banner
+  $('#lista-banner').nivoSlider({
+    pauseOnHover:true,
+    
+  }
+
+    
+  )
